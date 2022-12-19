4fdeead7d9c8f431f687e946fae7c31881907a85
Solomon <musolomoni@gmail.com>
2022-12-19 21:11:30 +0300

    Responding with text, html pages.

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const http = require("http");
+const fs = require("fs");
 
-const server = http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
 
-})
+  //Setting a header Type
+  res.setHeader("Content-Type", "text/html");
 
-server.listen(3000, 'localhost', () => {
-    
-})
+  //Sending an html file.
+
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      // res.write(data);
+      res.end(data);
+    }
+  });
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("Listening for requests on port 3000");
+});
