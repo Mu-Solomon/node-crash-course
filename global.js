9d48001f572a68ce33eea67cb272ef913d5fa459
Solomon <musolomoni@gmail.com>
2022-12-20 23:36:40 +0300

    Deleted unwanted files at the moment

--- global.js
+++
@@ -1,8 +0,0 @@
-setTimeout(() => {
-  console.log("This is a Timeout.");
-  clearInterval(int);
-}, 3000);
-
-let int = setInterval(() => {
-  console.log(__filename);
-}, 1000);
--- modules.js
+++
@@ -1,7 +0,0 @@
-const { people, carriers } = require("./people");
-
-console.log(people, carriers);
-
-const os = require("os");
-
-console.log(os.platform(), os.homedir());
