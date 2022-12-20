8caa90c824791fcd769ef15357a29188259dfb1d
Solomon <musolomoni@gmail.com>
2022-12-21 00:55:45 +0300

    Used ejs files instead of html files

--- app.js
+++ app.js
@@ -3,24 +3,25 @@ const express = require("express");
 //Setting up the express app.
 const app = express();
 
+//Registering the ejs engine
+app.set("view engine", "ejs");
+
 //Responding to requests just  as in node but in a much easier way.
 app.get("/", (req, res) => {
-  // res.send("<p> This is the homepage. </p>");
-  res.sendFile("./views/index.html", { root: __dirname });
-});
-app.get("/about", (req, res) => {
-  // res.send("<p> This is the about page. </p>");
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("index");
 });
 
-//Redirecting
-app.get("/about-me", (req, res) => {
-  res.redirect("/about");
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 //404 page (It must be at the very bottom)
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
 
 app.listen(3000);
