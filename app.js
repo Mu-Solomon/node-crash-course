6bc1967c76b55b87eee4ad7ebcc0ac998447b994
Solomon <musolomoni@gmail.com>
2022-12-20 06:17:01 +0300

    Html pages

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const app = express();
 
 //Responding to requests just  as in node but in a much easier way.
 app.get("/", (req, res) => {
-  res.send("<p> This is the homepage. </p>");
+ // res.send("<p> This is the homepage. </p>");
+  res.sendFile("./views/index.html", { root: __dirname });
+});
+app.get("/about", (req, res) => {
+ // res.send("<p> This is the about page. </p>");
+  res.sendFile("./views/about.html", { root: __dirname });
 });
 
 app.listen(3000);
