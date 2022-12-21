6defbd585273a40bbe0c33a156e08515ee0d2cf6
Solomon <musolomoni@gmail.com>
2022-12-21 09:24:39 +0300

    Using middleware

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const app = express();
 //Registering the ejs engine
 app.set("view engine", "ejs");
 
+//Middleware
+app.use((req, res) => {
+  console.log("request made:");
+  console.log("host:", req.hostname);
+  console.log("path:", req.path);
+  console.log("method:", req.method);
+});
+
 //Responding to requests just  as in node but in a much easier way.
 app.get("/", (req, res) => {
   const blogs = [
@@ -25,7 +33,7 @@ app.get("/", (req, res) => {
         " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos molestiae facere atque perferendis. Ipsam, accusantium vitae? Officia accusantium officiis dolor maxime cum corrupti error neque, commodi aliquam quo, eius odio voluptatibus vel veritatis! Odit odio vero, culpa sapiente consequatur nemo assumenda. Id deleniti vitae, amet veniam minus neque veritatis suscipit aut alias expedita quam eaque dolore quisquam hic modi provident voluptatibus nam minima dolor maxime corporis voluptate maiores eligendi. Quas, repellat facere! Hic sit eos impedit officia! Ipsam ea hic consequuntur optio exercitationem itaque explicabo eum aliquid maxime nesciunt necessitatibus, ex vitae rem corporis, iusto voluptatum. Autem perspiciatis cupiditate quaerat.",
     },
   ];
-  res.render("index", {blogs});
+  res.render("index", { blogs });
 });
 
 app.get("/about", (req, res) => {
