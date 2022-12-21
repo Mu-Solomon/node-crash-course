2fa21fca82f6c21ac5f61a103103154fe31d6e03
Solomon <musolomoni@gmail.com>
2022-12-21 08:14:19 +0300

    Dynamic data using ejs

--- app.js
+++ app.js
@@ -8,7 +8,24 @@ app.set("view engine", "ejs");
 
 //Responding to requests just  as in node but in a much easier way.
 app.get("/", (req, res) => {
-  res.render("index");
+  const blogs = [
+    {
+      title: "American whatsapp number ",
+      snippet:
+        " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos molestiae facere atque perferendis. Ipsam, accusantium vitae? Officia accusantium officiis dolor maxime cum corrupti error neque, commodi aliquam quo, eius odio voluptatibus vel veritatis! Odit odio vero, culpa sapiente consequatur nemo assumenda. Id deleniti vitae, amet veniam minus neque veritatis suscipit aut alias expedita quam eaque dolore quisquam hic modi provident voluptatibus nam minima dolor maxime corporis voluptate maiores eligendi. Quas, repellat facere! Hic sit eos impedit officia! Ipsam ea hic consequuntur optio exercitationem itaque explicabo eum aliquid maxime nesciunt necessitatibus, ex vitae rem corporis, iusto voluptatum. Autem perspiciatis cupiditate quaerat.",
+    },
+    {
+      title: " Auto reply robot",
+      snippet:
+        " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos molestiae facere atque perferendis. Ipsam, accusantium vitae? Officia accusantium officiis dolor maxime cum corrupti error neque, commodi aliquam quo, eius odio voluptatibus vel veritatis! Odit odio vero, culpa sapiente consequatur nemo assumenda. Id deleniti vitae, amet veniam minus neque veritatis suscipit aut alias expedita quam eaque dolore quisquam hic modi provident voluptatibus nam minima dolor maxime corporis voluptate maiores eligendi. Quas, repellat facere! Hic sit eos impedit officia! Ipsam ea hic consequuntur optio exercitationem itaque explicabo eum aliquid maxime nesciunt necessitatibus, ex vitae rem corporis, iusto voluptatum. Autem perspiciatis cupiditate quaerat.",
+    },
+    {
+      title: " Make money online",
+      snippet:
+        " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos molestiae facere atque perferendis. Ipsam, accusantium vitae? Officia accusantium officiis dolor maxime cum corrupti error neque, commodi aliquam quo, eius odio voluptatibus vel veritatis! Odit odio vero, culpa sapiente consequatur nemo assumenda. Id deleniti vitae, amet veniam minus neque veritatis suscipit aut alias expedita quam eaque dolore quisquam hic modi provident voluptatibus nam minima dolor maxime corporis voluptate maiores eligendi. Quas, repellat facere! Hic sit eos impedit officia! Ipsam ea hic consequuntur optio exercitationem itaque explicabo eum aliquid maxime nesciunt necessitatibus, ex vitae rem corporis, iusto voluptatum. Autem perspiciatis cupiditate quaerat.",
+    },
+  ];
+  res.render("index", {blogs});
 });
 
 app.get("/about", (req, res) => {
